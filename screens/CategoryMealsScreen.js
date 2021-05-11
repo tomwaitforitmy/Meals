a8ce6de27ea432c6c8333f2f22d382389bb0bf10
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-05-11 14:24:51 +0200

    Dynamic header for CategoryMealsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import Colors from "../constants/Colors";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
+  const categoryId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === categoryId);
+
   return (
     <View style={{ ...styles.container, ...props.style }}>
-      <Text>CategoryMealsScreen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meal Details"
         onPress={() => {
@@ -21,6 +26,19 @@ const CategoryMealsScreen = (props) => {
       ></Button>
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const categoryId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === categoryId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Colors.primary,
+    },
+    headerTintColor: "white",
+  };
 };
 
 const styles = StyleSheet.create({
