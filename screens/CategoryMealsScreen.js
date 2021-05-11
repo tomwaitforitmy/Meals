28b2a1becdb540d8f58ae1a508e003dc96c3fea5
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-05-11 20:07:56 +0200

    Add meal data

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,28 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
   const categoryId = props.navigation.getParam("categoryId");
   const selectedCategory = CATEGORIES.find((cat) => cat.id === categoryId);
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={{ ...styles.container, ...props.style }}>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Go to Meal Details"
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: "MealDetail",
-          });
-        }}
-      ></Button>
-      <Button
-        title="Back"
-        onPress={() => {
-          props.navigation.goBack();
-        }}
-      ></Button>
+      <FlatList
+        data={displayedMeals}
+        // onPress={() => {
+        //   props.navigation.navigate({ routeName: "MealDetail" });
+        // }}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
