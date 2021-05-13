2fe39e599fbd06746e8efc03f7c06d052030ece3
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-05-13 19:54:28 +0200

    Fixed crash in header button + fixed color

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -9,7 +9,7 @@ const CustomHeaderButton = (props) => {
       {...props}
       IconComponent={Ionicons}
       iconSize={23}
-      color={Colors.primary}
+      color={Colors.favoriteSelected}
     />
   );
 };
