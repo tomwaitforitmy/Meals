c0e601053bf774af9374a8dd94222d5a5c76566b
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-05-16 16:44:51 +0200

    Add ListItem and show ingredients + steps in MealDetails

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import DefaultText from "../components/DefaultText";
 import HeaderButton from "../components/HeaderButton";
 import { MEALS } from "../data/dummy-data";
+
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
   const meal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={{ ...styles.container, ...props.style }}>
-      <Text>{meal.title}</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={{ uri: meal.imageUrl }} style={styles.image}></Image>
+      <View style={styles.details}>
+        <DefaultText>{meal.duration}m</DefaultText>
+        <DefaultText>{meal.complexity}</DefaultText>
+        <DefaultText>{meal.affordability}</DefaultText>
+      </View>
+      <DefaultText style={styles.title}>Ingredients</DefaultText>
+      {meal.ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <DefaultText style={styles.title}>Steps</DefaultText>
+      {meal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
@@ -40,7 +62,30 @@ MealDetailScreen.navigationOptions = (props) => {
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  container: {
+    flex: 1,
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
 export default MealDetailScreen;
